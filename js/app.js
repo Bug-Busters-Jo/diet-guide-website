e2e238928cb0e40db7898ef78c91f2fa68fa3b46
areejobaid94 <areej.obaid@yahoo.com>
2021-01-18 10:23:32 +0200

    log page

---
+++ js/app.js
@@ -0,0 +1,34 @@
+"use strict"
+let user = function(name, email, pass,waigth,mobile, height ){
+    this.name = name;
+    this.email = email;
+    this.password = pass;
+    this.waigth = waigth;
+    this.mobile =mobile;
+    this.height = height;
+};
+
+
+var userDate = localStorage.getItem('logedUser');
+window.onload = function(){
+    var loginLogout = document.getElementById('login-logout');
+    if(userDate != null){
+        userDate = JSON.parse(userDate);
+        loginLogout.textContent = 'Log Out'
+    }else{
+        loginLogout.textContent = 'Log In'
+    }
+}
+
+function loginLogout(){
+    if(userDate == null){
+        window.location.href='dataForm.html';
+    }else{
+        let isConfirm = confirm('Are You Sure');
+        if(isConfirm){
+            localStorage.removeItem('logedUser');
+            alert('Done!');
+            window.location.href='index.html';
+        }
+    }
+}
