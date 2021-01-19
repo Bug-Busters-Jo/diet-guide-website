e0bd3d6278b5b7201b2ae5d3af61280c37c8c3be
areejobaid94 <areej.obaid@yahoo.com>
2021-01-19 21:48:06 +0200

    add-todo-list

--- js/toDoList.js
+++ js/toDoList.js
@@ -1,15 +1,4 @@
 "use strict"
-
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
-
 // Click on a close button to hide the current list item
 var close = document.getElementsByClassName("close");
 var i;
@@ -32,6 +21,7 @@ list.addEventListener('click', function(ev) {
 function newElement() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
+  li.setAttribute('class','li');
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
